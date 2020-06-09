0aefe74419a1c05b969739816f06de723635817d
Jean Luc GAMAN <43534237+jgaman@users.noreply.github.com>
2020-06-09 10:31:46 +0000

    Form
    Ajout d'une form

--- Emprunt/Emprunt/Program.cs
+++ Emprunt/Emprunt/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace Emprunt
 {
@@ -10,6 +11,33 @@ namespace Emprunt
     {
         static void Main(string[] args)
         {
+            //montant emprunté
+            double montant;
+            Console.Write("Tapez le montant à emprunter (ex: 10000) :");
+            string strMontant = Console.ReadLine();
+            montant = Convert.ToDouble(strMontant);
+
+            //taux annuel
+            double taux;
+            Console.Write("taux (pour 3,2% tapez 3,2) :");
+            string srtTaux = Console.ReadLine();
+            taux = Convert.ToDouble(srtTaux);
+
+            //durée en années 
+            int durée;
+            Console.Write("durée en années (ex: 10) :");
+            string strDurée = Console.ReadLine();
+            durée = Convert.ToInt32(strDurée);
+
+            //montnt du remboursement mensuel
+            double échéance = Financial.Pmt(taux / 12, durée * 12, montant);
+
+            //format P=percent N=number C=currency
+            Console.WriteLine("mensualité en € {1:C2} avec taux {0:N2}% ", taux, -échéance);
+            Console.ReadLine();
+                
+
         }
+
     }
 }
