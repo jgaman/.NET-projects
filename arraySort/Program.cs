e219c69776f2ccfb7bbf4cc832d4a09322e15d46
jean luc gaman <gaman2404@gmail.com>
2020-06-17 23:43:47 +0200

    playground

--- arraySort/Program.cs
+++ arraySort/Program.cs
@@ -30,6 +30,24 @@ namespace arraySort
             {
                 Console.WriteLine($"-- {pallet}");
             }
+            Array.Clear(pallets, 0, 2);
+            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine("");
+            Array.Resize(ref pallets, 6);
+            Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+            pallets[4] = "C01";
+            pallets[5] = "C02";
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
         }
     }
 }
