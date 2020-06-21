119dc03eff7555f8d29150c6319ebea6fdca1900
jean luc gaman <gaman2404@gmail.com>
2020-06-21 19:55:43 +0200

    mgicNumber Loop exercice

---
+++ magicNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace magicNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
