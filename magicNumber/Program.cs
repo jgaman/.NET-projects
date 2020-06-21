6e46655f4cf057d93ead860b09c4f63567b781b4
jean luc gaman <gaman2404@gmail.com>
2020-06-21 20:19:47 +0200

    ajout de boucles

--- magicNumber/Program.cs
+++ magicNumber/Program.cs
@@ -6,7 +6,38 @@ namespace magicNumber
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int magicNumber = 5;
+            int nombreUtilisateur = 0;
+            int nombreDeVies = 4;
+
+            while (nombreUtilisateur > 0)
+            {
+
+      
+
+            Console.WriteLine("Devinez le nombre magique:");
+            String resultat = Console.ReadLine();
+
+            if (int.TryParse(resultat, out nombreUtilisateur))
+                //La conversion s'est bien passée
+
+            {
+                if (nombreUtilisateur < magicNumber)
+                {
+                    Console.WriteLine("Le nombre magique est plus grand.");
+                }
+                else if (nombreUtilisateur > magicNumber)
+                {
+                    Console.WriteLine("Le nombre magique est plus petit.");
+                }
+                else
+                {
+                    //egalité on a gagné
+                    Console.WriteLine("Bravo: Vous vez trouvé le nombre magique");
+                }
+            }
+
         }
+    }
     }
 }
