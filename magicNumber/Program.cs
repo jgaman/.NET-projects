037df29efbf88bfa274dedb9f3ce27229d4567b3
jean luc gaman <gaman2404@gmail.com>
2020-06-21 20:40:51 +0200

    jout de vie et de la boucle

--- magicNumber/Program.cs
+++ magicNumber/Program.cs
@@ -1,43 +1,55 @@
 ﻿using System;
 
-namespace magicNumber
+namespace nb_magique_console
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            int magicNumber = 5;
-            int nombreUtilisateur = 0;
-            int nombreDeVies = 4;
+            int nombreDeVies = 0;
+            int nombreMagique = 5;
+            const int NOMBRE_DE_VIES = 3;
 
-            while (nombreUtilisateur > 0)
+            for (nombreDeVies = NOMBRE_DE_VIES; nombreDeVies > 0; nombreDeVies--)
             {
 
-      
+                Console.Write("Devinez le nombre magique (il vous reste " + nombreDeVies + " vies): ");
+                String resultat = Console.ReadLine();
 
-            Console.WriteLine("Devinez le nombre magique:");
-            String resultat = Console.ReadLine();
-
-            if (int.TryParse(resultat, out nombreUtilisateur))
-                //La conversion s'est bien passée
-
-            {
-                if (nombreUtilisateur < magicNumber)
+                int nombreUtilisateur = 0;
+                if (int.TryParse(resultat, out nombreUtilisateur))
                 {
-                    Console.WriteLine("Le nombre magique est plus grand.");
-                }
-                else if (nombreUtilisateur > magicNumber)
-                {
-                    Console.WriteLine("Le nombre magique est plus petit.");
+                    // La convertion s'est bien passée
+
+                    if (nombreUtilisateur < nombreMagique)
+                    {
+                        Console.WriteLine("Le nombre magique est plus grand.");
+                    }
+                    else if (nombreUtilisateur > nombreMagique)
+                    {
+                        Console.WriteLine("Le nombre magique est plus petit.");
+                    }
+                    else
+                    {
+                        // Egalité : on a gagné
+                        Console.WriteLine("BRAVO: Vous avez trouvé le nombre magique");
+                        break;
+                    }
                 }
                 else
                 {
-                    //egalité on a gagné
-                    Console.WriteLine("Bravo: Vous vez trouvé le nombre magique");
+                    // Erreur de la conversion
+                    Console.WriteLine("ERREUR: Vous devez rentrer un nombre.");
                 }
+
+                Console.WriteLine("");
             }
 
+            // Sortie de la boucle
+            if (nombreDeVies == 0)
+            {
+                Console.WriteLine("Désolé, vous avez perdu.");
+            }
         }
-    }
     }
 }
